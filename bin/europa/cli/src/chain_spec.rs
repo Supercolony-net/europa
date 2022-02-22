c7a45a3212a36cd163cfc9fe6831d7fc3e09434b
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2022-02-22 03:49:12 +0200

    initial balance of 1 << 127

--- bin/europa/cli/src/chain_spec.rs
+++ bin/europa/cli/src/chain_spec.rs
@@ -81,11 +81,11 @@ fn genesis(root_key: AccountId, endowed_accounts: Vec<AccountId>) -> GenesisConf
 			changes_trie_config: Default::default(),
 		},
 		balances: BalancesConfig {
-			// Configure endowed accounts with initial balance of 1 << 60.
+			// Configure endowed accounts with initial balance of 1 << 127.
 			balances: endowed_accounts
 				.iter()
 				.cloned()
-				.map(|k| (k, 1 << 60))
+				.map(|k| (k, 1 << 127))
 				.collect(),
 		},
 		sudo: SudoConfig {
